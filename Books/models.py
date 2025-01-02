aa6d29631aaa5e07d96ba8dfa995e67fd2b318e6
shihab <shihabshahriar543@gmail.com>
2025-01-02 19:55:25 +0600

    full project push for hosting in render

---
+++ Books/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+from Categories.models import Category
+
+# Create your models here.
+class Book(models.Model):
+    category = models.ManyToManyField(Category)
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=300)
+    image = models.ImageField(upload_to='books/', blank=True, null=True)
+    borrowing_users = models.ManyToManyField(User, related_name='borrow_history',blank=True)
+    price = models.DecimalField(max_digits=4 , decimal_places= 2,default=0)
+
+    def __str__(self) -> str:
+        return f'Book Title : {self.title}'
---
+++ Books/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Book
+# Create your views here.
+
+
+class BookDetailView(DetailView):
+    model = Book
+    pk_url_kwarg = 'id'
+    template_name = 'bookDetails.html'
+    context_object_name = 'book'
